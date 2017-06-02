f2870e8dcf1b2cbc219ce10f93ec7cbe3ee64a28
Dhananjay <jai4uraj@gmail.com>
2017-06-02 20:14:36 +0530

    More commits

--- react-jasmine-karma-bdd/src/Artist.js
+++ react-jasmine-karma-bdd/src/Artist.js
@@ -3,32 +3,79 @@ import React, {Component} from 'react';
 class Artist extends Component {
     constructor() {
         super();
-        //this.state = [{x: '2', y: '2'}]
-        this.state = {addresult: []}
-        this.state = {subsresult: []}
+        //this.state = [{x: '', y: ''}]
     }
 
+    valdate(x,y){
+
+        var numX = parseInt(x);
+        var numY = parseInt(y);
+
+        if(numX != '' && numY != '')
+        {
+            return true
+        }
+        if(numX != 0 && numY != 0)
+        {
+            return true
+        }
+        if(numX !=parseInt(numX) && numY !=parseInt(numY) )
+        {
+            return true
+        }
+        if(numX !=parseFloat(numX) && numY !=parseFloat(numY) )
+        {
+            return true
+        }
+        if(numX > 0 && numY > 0) {
+            return true
+        }
+        if(parseInt(numX) && parseInt(numY)){
+            return true
+        }
+    }
 
     add(x, y) {
-        return parseInt(x) + parseInt(y);
+        if(this.valdate(x, y)) {
+            return x + y;
+        }
     }
 
     subtract(x,y) {
-        return parseInt(x) - parseInt(y);
+        if(this.valdate(x, y)) {
+            return x - y;
+        }
     }
 
+
+    multiply(x,y) {
+        if(this.valdate(x, y)) {
+            return x * y;
+        }
+}
+
+    divide(x, y) {
+    if(this.valdate(x, y)) {
+        return (y === 0) ? 0 : x / y;
+    }
+    else {
+        return 0;
+    }
+}
+
+
+
     render() {
-        //console.log("props x = ",this.props.x);
-        //console.log("props y = ",this.props.y);
-/*
+        /*
+        console.log("props x = ",this.props.x);
+        console.log("props y = ",this.props.y);
         var resultAdd = this.add(this.props.x, this.props.y);
         this.state.addresult = resultAdd;
         var resultSubs = this.subtract(this.props.x, this.props.y);
         this.state.subsresult = resultSubs;
 */
-
         return (
-                <span>This is some static text</span>
+            <div>Artist name : {this.props.name}</div>
         );
     }
 }
--- react-jasmine-karma-bdd/test/ArtistSpec.js
+++ react-jasmine-karma-bdd/test/ArtistSpec.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
-//import TestUtils from 'react-addons-test-utils';
+import TestUtils from 'react-addons-test-utils';
 
 import Artist from '../src/Artist';
 
@@ -8,21 +8,21 @@ import Artist from '../src/Artist';
 
 describe('Artist', () => {
 
-/*    it("Should display the correct Artist Name", () => {
+    it("Should display the correct Artist Name", () => {
         var renderer = TestUtils.createRenderer();
         var label = renderer.render(<Artist name="Dhananjay" />);
-        //console.log("props = ",label.props.children[1]);
+        console.log("Artist props = ",label.props.children[1]);
         expect(label.props.children[1]).toBe("Dhananjay");
     });
 
     it("Should display the incorrect Artist Name", () => {
         var renderer = TestUtils.createRenderer();
         var label = renderer.render(<Artist name="Dhananjay123" />);
-        //console.log("props = ",label.props.children[1]);
+        console.log("Your props = ",label.props.children[1]);
         expect(label.props.children[1]).toBe("Dhananjay123");
     });
 
-    it("Should display the add result", () => {
+    /*it("Should display the add result", () => {
         var renderer = TestUtils.createRenderer();
         var label = renderer.render(<Artist x="2" y="2" />);
         //var result = label.add(2,2);
@@ -33,17 +33,36 @@ describe('Artist', () => {
     it("Should display the add result", () => {
         var label = ReactTestUtils.renderIntoDocument(<Artist x="2" y="2"/>);
         var addResult = label.add(2,2);
-        console.log("label = "+addResult);
-        //expect(label.state.addresult).toBe(4);
+        console.log("Add = "+addResult);
         expect(addResult).toBe(4);
     });
 
     it("Should display the substract result", () => {
         var label = ReactTestUtils.renderIntoDocument(<Artist x="2" y="2"/>);
         var subsResult = label.subtract(2,2);
-        console.log("label = "+subsResult);
-        //expect(label.state.subsresult).toBe(0);
+        console.log("Substract = "+subsResult);
         expect(subsResult).toBe(0);
+    });
+
+    it("should multiply two numbers together result 10x2 =20 ", () => {
+        var label = ReactTestUtils.renderIntoDocument(<Artist x="10" y="2"/>);
+        var multiplyResult = label.multiply(10,2);
+        console.log("Multiply = "+multiplyResult);
+        expect(multiplyResult).toBe(20);
+    });
+
+    it("should divide correctly result 10/2 =2 ", () => {
+        var label = ReactTestUtils.renderIntoDocument(<Artist x="10" y="2"/>);
+        var divideResult = label.divide(10,2);
+        console.log("Divide = "+divideResult);
+        expect(divideResult).toBe(5);
+    });
+
+    it("should return 0 when dividing by 0 result 10/0 =0 ", () => {
+        var label = ReactTestUtils.renderIntoDocument(<Artist x="10" y="0"/>);
+        var divideResult = label.divide(10,0);
+        console.log("Divide 0 = "+divideResult);
+        expect(divideResult).toBe(0);
     });
 
 });
